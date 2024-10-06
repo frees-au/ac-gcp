56b95c9f659654226498b493a383b6ce95110dd3
Simon Hobbs <simon@hobbs.id.au>
2024-10-06 13:53:24 +1100

    Use dotenv for the python scripts.

--- python-scripts/stop-watch.py
+++ python-scripts/stop-watch.py
@@ -1,20 +1,21 @@
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
+from dotenv import dotenv_values
+
+cfg = dotenv_values(".env")
+
+print("With configuration:")
+for key, value in cfg.items():
+    print(f"{key}: {value}")
 
 # Load your service account credentials
 SCOPES = [
     'https://www.googleapis.com/auth/gmail.modify',
     'https://www.googleapis.com/auth/gmail.readonly'
 ]
-SERVICE_ACCOUNT_FILE = 'service-account.json'
-
 credentials = service_account.Credentials.from_service_account_file(
-    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-
-# Impersonate the user
-delegated_credentials = credentials.with_subject('REDACTED@example.com')
-
-# Build the Gmail service
+    cfg["GMAIL_SERVICE_ACCOUNT"], scopes=SCOPES)
+delegated_credentials = credentials.with_subject(cfg["GMAIL_ACCOUNT_EMAIL"])
 service = build('gmail', 'v1', credentials=delegated_credentials)
 
 # Stop watching the mailbox
--- python-scripts/watch.py
+++ python-scripts/watch.py
@@ -1,31 +1,28 @@
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
+from dotenv import dotenv_values
 
-# Load your service account credentials
+cfg = dotenv_values(".env")
+
+print("With configuration:")
+for key, value in cfg.items():
+    print(f"{key}: {value}")
+
+# Impersonate the user with scopess.
 SCOPES = [
     'https://www.googleapis.com/auth/gmail.modify',
     'https://www.googleapis.com/auth/gmail.readonly'
 ]
-SERVICE_ACCOUNT_FILE = 'service-account.json'
-
 credentials = service_account.Credentials.from_service_account_file(
-    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-
-# Impersonate the user
-delegated_credentials = credentials.with_subject('REDACTED@example.com')
-
-# Build the Gmail service
+    cfg["GMAIL_SERVICE_ACCOUNT"], scopes=SCOPES)
+delegated_credentials = credentials.with_subject(cfg["GMAIL_ACCOUNT_EMAIL"])
 service = build('gmail', 'v1', credentials=delegated_credentials)
-# Use list-labels.py to list you label IDs.
-label_id = 'LABEL_REDACTED'
-# GCP Pub/Sub topic
-topic_name = 'projects/PROJECT/topics/NAME'
 
-# Set up the watch for the "ops-todo" label
+# Set up the watch for the label and topic.
 request = {
-    'labelIds': [label_id],
+    'labelIds': [cfg["GMAIL_QUEUE_LABEL"]],
     'labelFilterBehavior': 'include',
-    'topicName': topic_name
+    'topicName': cfg["PUBSUB_TOPIC"]
 }
 response = service.users().watch(userId='me', body=request).execute()
 
